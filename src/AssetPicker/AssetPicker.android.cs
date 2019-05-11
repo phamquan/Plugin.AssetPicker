d9a38ac4556a4f6771b823d7b2d6b281c96dbeb9
QuanP <mr.pquan@gmail.com>
2019-05-11 07:30:18 +0700

    init plugin project

---
+++ src/AssetPicker/AssetPicker.android.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plugin.AssetPicker
+{
+    /// <summary>
+    /// Interface for AssetPicker
+    /// </summary>
+    public class AssetPickerImplementation : IAssetPicker
+    {
+    }
+}
---
+++ src/AssetPicker/IAssetPicker.shared.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plugin.AssetPicker
+{
+    public interface IAssetPicker
+    {
+    }
+}
